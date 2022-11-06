dabea750416a7ff8e82dd5ba939c40efd9bf49b0
jones-bass <jonesbass.tb@gmail.com>
2022-11-06 15:47:42 -0300

    Iniciando Projeto DIO

---
+++ formulariodio/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+  },
+};
---
+++ formulariodio/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      Ola BB
+    </div>
+  );
+}
+
+export default App;
